2ebfb67b1320095ff1011f2147f38dbb68cb4f29
ArthurHab <arthur200106@gmail.com>
2023-06-17 17:59:58 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponentComponent } from './components/testing/first-component/first-component.component';
+import { SecondComponentComponent } from './components/testing/second-component/second-component.component';
+import { ThirdComponentComponent } from './components/testing/third-component/third-component.component';
+import { FourthComponentComponent } from './components/testing/fourth-component/fourth-component.component';
+import { FifthComponentComponent } from './components/testing/fifth-component/fifth-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponentComponent,
+    SecondComponentComponent,
+    ThirdComponentComponent,
+    FourthComponentComponent,
+    FifthComponentComponent,
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
