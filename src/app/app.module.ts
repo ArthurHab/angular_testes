3e553f2101486d4703ece3f6863eac2fc01050e0
ArthurHab <arthur200106@gmail.com>
2023-06-19 10:05:22 -0300

    new components and pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,14 @@ import { SecondComponentComponent } from './components/testing/second-component/
 import { ThirdComponentComponent } from './components/testing/third-component/third-component.component';
 import { FourthComponentComponent } from './components/testing/fourth-component/fourth-component.component';
 import { FifthComponentComponent } from './components/testing/fifth-component/fifth-component.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { BaseComponent } from './components/layout/base/base.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { NavbarComponent } from './components/layout/navbar/navbar.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { ContactComponent } from './components/pages/contact/contact.component';
+import { NewsComponent } from './components/pages/news/news.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +26,14 @@ import { FifthComponentComponent } from './components/testing/fifth-component/fi
     ThirdComponentComponent,
     FourthComponentComponent,
     FifthComponentComponent,
+    HomeComponent,
+    HeaderComponent,
+    BaseComponent,
+    FooterComponent,
+    NavbarComponent,
+    AboutComponent,
+    ContactComponent,
+    NewsComponent
   ],
   imports: [
     BrowserModule,
